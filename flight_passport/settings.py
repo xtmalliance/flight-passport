0872aff91e81339c62d6ceca18d15d8a39da221b
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2020-09-28 13:54:51 +0100

    Update settings.py

--- flight_passport/settings.py
+++ flight_passport/settings.py
@@ -27,7 +27,7 @@ if ENV_FILE:
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -102,8 +102,8 @@ LOGO_URL = 'https://about.openskies.sh/images/logo.svg'
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
-# if DEBUG:
-    # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 EMAIL_HOST = 'smtp.sendgrid.net'
