665310c6decc6c520e7cbb137a687244fbf3f557
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-10-22 20:45:39 +0100

    Create oauth_validators.py

---
+++ authprofiles/oauth_validators.py
@@ -0,0 +1,13 @@
+from oauth2_provider.oauth2_validators import OAuth2Validator
+   
+class PassportOAuth2Validator(OAuth2Validator):
+    def get_additional_claims(self, request):
+        return {
+            "sub": request.user.email,
+            "first_name": request.user.first_name,
+            "last_name": request.user.last_name,
+        }
+        
+    def get_userinfo_claims(self, request):
+        claims = super().get_userinfo_claims()
+        return claims
