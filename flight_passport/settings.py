323e20b8c97cc9436b23a5c4d6d781bed6fb2d35
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2022-02-18 12:33:44 +0000

    Update settings.py

--- flight_passport/settings.py
+++ flight_passport/settings.py
@@ -27,7 +27,7 @@ if ENV_FILE:
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
