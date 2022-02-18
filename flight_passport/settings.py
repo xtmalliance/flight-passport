c5c383da713ef4614dc5a51e60f926b9f0a941e1
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2022-02-18 12:44:19 +0000

    Updated requirements to bump idna

--- flight_passport/settings.py
+++ flight_passport/settings.py
@@ -27,7 +27,7 @@ if ENV_FILE:
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
