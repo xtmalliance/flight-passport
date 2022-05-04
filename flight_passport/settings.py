f9f3a0e68c579826305cbd3e58a14a87fac6de42
Hrishikesh Ballal <hballal@gmail.com>
2022-05-04 21:03:11 +0100

    Remove django-heroku

--- flight_passport/settings.py
+++ flight_passport/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 from dotenv import load_dotenv, find_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -243,4 +243,4 @@ LOGGING = {
         },
     }
 }
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
