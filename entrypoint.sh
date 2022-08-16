d684ae53b7284bbf9e0d88c21386bfe4f215835b
hrishiballal <hrishikeshballal@yahoo.com>
2022-08-16 17:51:49 +0100

    Updated Database URL

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ python manage.py migrate --noinput
 echo "Creating superuser.."
 python manage.py createsuperuser --noinput
 echo "Collecting static"
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 # Start server
 echo "Starting server.."
 python manage.py runserver 0.0.0.0:8000
